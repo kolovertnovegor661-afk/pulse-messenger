778621027b76e0bf8d12a5f1f72cd222398a4c33
kolovertnovegor661-afk <kolovertnovegor661@gmail.com>
2026-03-28 08:59:00 +0500

    firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) throw UnsupportedError('Web not supported');
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError('Platform not supported');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDvEjiGaLyFle9rEBLeTB9Pa1PtqToYuN0',
+    appId: '1:129265111480:android:b503b2f78baee06e733f4d',
+    messagingSenderId: '129265111480',
+    projectId: 'pulse-6e20a',
+    storageBucket: 'pulse-6e20a.firebasestorage.app',
+    databaseURL: 'https://pulse-6e20a-default-rtdb.firebaseio.com',
+  );
+}
